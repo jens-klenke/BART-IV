d7cb1fcb79cec697827eb5e013debb14402e0d54
Klenke <57100601+jens-klenke@users.noreply.github.com>
2024-04-08 16:59:04 +0200

    desk analyse

--- 01_code/desk_stats.R
+++ 01_code/desk_stats.R
@@ -8,8 +8,9 @@ invisible(sapply(list.files(here::here('01_code/functions'), full.names = TRUE),
                  source))
 
 ## load data
-# reading data 
-data <- readRDS(here::here('03_sim_results/baseline_mu.rds'))
+### Data to big for Github -> download from 
+# data <- readRDS(here::here('03_sim_results/baseline_mu.rds'))
+data <- readRDS(url('https://uni-duisburg-essen.sciebo.de/s/Rt7sNPFwaVw1lr3/download'))
 
 df <- data %>% 
   dplyr::slice_head(n = 1) %>%
@@ -20,18 +21,40 @@ df <- data %>%
   dplyr::mutate(ID = readr::parse_number(
     str_extract(path_in, "_(?:.(?!_))+$"))) %>%
   # delselect unimportant variables
-  dplyr::select(-c(row_num, path_in))
-  
+  dplyr::select(-c(row_num, path_in)) %>%
+  dplyr::filter(stringi::stri_detect(result_names, regex = 'bcf_exp'))
 
-names(df$results)
-   
-  
-  
-  
-  
-df <- cbind.data.frame(dataset$y, dataset$X)
 
-colnames(df) <- c('y', paste0('x', 1:ncol(dataset$X)))
+df_1 <- df %>%
+  # dplyr::slice_head(n = 1) %>%
+#  dplyr::filter(result_names == 's_bcf_exp') %>%
+  # unnest results to get the individual estimates 
+  tidyr::unnest(results) %>%
+  # code subgroups
+  dplyr::mutate(subgroups = dplyr::case_when(
+    x1 == 0 & x2 == 0 ~ 'positive_effect',
+    x1 == 1 & x2 == 1 ~ 'negative_effect',
+    .default = NA
+  )) %>%
+  # filter for subgroups
+  dplyr::filter(!is.na(subgroups)) %>%
+  dplyr::mutate(tauhat = dplyr::case_when(
+    result_names == 'bcf_exp' ~ bcf_tauhat,
+    result_names == 's_bcf_exp' ~ s_bcf_tauhat,
+  )) %>%
+  # group by method and dataset
+  dplyr::group_by(result_names, subgroups, ID) %>%
+  dplyr::summarise(mean = mean(tauhat), sd = sd(tauhat),
+                   median = median(tauhat), min = min(tauhat),
+                   max = max(tauhat))
+
+df_1 
+
+
+
+
+  
+  
 
 # mean of y
 df %>%
