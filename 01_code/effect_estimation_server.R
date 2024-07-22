10ea40f5bdedb4fdd32001db2a2dd50645479ef8
Klenke <57100601+jens-klenke@users.noreply.github.com>
2024-07-22 08:35:17 +0200

    server script

--- 01_code/effect_estimation_server.R
+++ 01_code/effect_estimation_server.R
@@ -8,16 +8,17 @@ invisible(sapply(list.files(here::here('01_code/functions'), full.names = TRUE),
                  source))
 
 # parallel plan
-future::plan(multisession, workers = base::floor(parallel::detectCores()*0.5))
+future::plan(multisession, workers = 40) # base::floor(parallel::detectCores()*0.75))
 options(future.globals.maxSize = 2147483648) # 2GB  
 
-# # reading data 
+# reading data -----
 data <- tibble::tibble(
   # path for loading data
   path_in = list.files(
     here::here("00_sim_data/"), recursive = TRUE, full.names = TRUE)
-) %>%
-  dplyr::filter(str_detect(path_in, 'correlated')) %>%
+  ) %>%
+  # filtering for desired data
+  dplyr::filter(str_detect(path_in, 'correlated_confounded')) %>%
   dplyr::mutate(ncov = readr::parse_number(stringr::str_extract(path_in, pattern = 'ncovs[0-9]*')),
                 row_num = paste(dplyr::row_number(), 'of', max(dplyr::row_number())))
 
@@ -32,7 +33,7 @@ tictoc::toc()
 #   dplyr::mutate(results = purrr::pmap(., wrapper_function, .progress = TRUE))
 # tictoc::toc()
 
-save(sim_results, file ='Z:/Data/baseline_try_correlated.RData')
+save(sim_results, file ='Z:/Data/baseline_try_correlated_confounded.RData')
 
 quit()
 
