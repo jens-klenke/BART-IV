547c522e67a895ca2a625ff1339372801ab91c12
Lennard <lennard.massmann@uni-due.de>
2024-07-24 23:05:21 +0200

    test for using causal rule ensemble (random forest) instead of binary tree for subgroup discovery. implemented softbart instead of post-lasso within select_rules as post-lasso shrinks all rules to zero (too large lambda?). test on filter_ functions necessary. too restrictive?

--- 01_code/test_SBCF_CRE.R
+++ 01_code/test_SBCF_CRE.R
@@ -16,12 +16,14 @@ source(here::here('01_code/packages.R'))
 # we need variables y, w, z, x
 
 # num covariates
-p <- 25
+p <- 10
 # num obs 
 n <- 1000
 # effects
 effect_size <- 2
 null <- 0
+
+
 
 # generate covariates
 get_features <- function(N, P, uncorrelated=T) {
@@ -49,8 +51,9 @@ get_features <- function(N, P, uncorrelated=T) {
   
   # Transform in continuous and binary covariates
   X = matrix(NA, N, P)
-  X[, 1:10] = qnorm(unif[, 1:10])
-  X[, 11:P] = qbinom(unif[, 11:P], 1, 0.3)
+  P_half <- round(P/2)
+  X[, 1:P_half] = qnorm(unif[, (1:P_half)])
+  X[, (P_half+1):P] = qbinom(unif[, ((P_half+1):P)], 1, 0.3)
   
   return(X)
   
@@ -75,38 +78,38 @@ w0 <- numeric(n)
 
 # baseline effect
 # 6 covariates are relevant for mu()
-# X1, X2, X3, X4, X21, X22
-mu = 3 + 1.5*sin(pi*X[, 1]) + 0.5*(X[, 2] - 0.5)^2 + 1.5*(2-abs(X[, 3])) + 1.5*X[, 4]*(0.5*(X[, 21] + 1)+0.5*(X[, 22] + 1))
+# X1, X2, X6, X7
+mu = 3 + 1.5*sin(pi*X[, 1]) + 0.5*(X[, 2] - 0.5)^2 + 1.5*(2-abs(X[, 6])) + 0.5*(X[, 7] + 1) # without + 1.5*X[, 4]*(0.5*(X[, 21] + 1)
 
 # Generate unit level potential outcome
 y0 <- mu + rnorm(n)
 y1 <- numeric(n)
 
-# x21 and x22 needed for heterogeneous effects
-x21 <- X[, 21]
-x22 <- X[, 22]
+# x6 and x7 needed for heterogeneous effects
+x3 <- X[, 6]
+x4 <- X[, 7]
 
-# Generate Heterogeneity - only depends on x21 and x22  
-y1[x21 == 0 & x22 == 0] <- y0[x21 == 0 & x22 == 0] + w1[x21 == 0 & x22 == 0] * effect_size
-y1[x21 == 0 & x22 == 1] <- y0[x21 == 0 & x22 == 1] + w1[x21 == 0 & x22 == 1] * null
-y1[x21 == 1 & x22 == 0] <- y0[x21 == 1 & x22 == 0] + w1[x21 == 1 & x22 == 0] * null
-y1[x21 == 1 & x22 == 1] <- y0[x21 == 1 & x22 == 1] + w1[x21 == 1 & x22 == 1] * -effect_size
+# Generate Heterogeneity - only depends on x3 and x4  
+y1[x3 == 0 & x4 == 0] <- y0[x3 == 0 & x4 == 0] + w1[x3 == 0 & x4 == 0] * effect_size
+y1[x3 == 0 & x4 == 1] <- y0[x3 == 0 & x4 == 1] + w1[x3 == 0 & x4 == 1] * null
+y1[x3 == 1 & x4 == 0] <- y0[x3 == 1 & x4 == 0] + w1[x3 == 1 & x4 == 0] * null
+y1[x3 == 1 & x4 == 1] <- y0[x3 == 1 & x4 == 1] + w1[x3 == 1 & x4 == 1] * -effect_size
 
 tau_true <- numeric(n)
-tau_true[x21 == 0 & x22 == 0] <- w1[x21 == 0 & x22 == 0] * effect_size
-tau_true[x21 == 0 & x22 == 1] <- w1[x21 == 0 & x22 == 1] * null
-tau_true[x21 == 1 & x22 == 0] <- w1[x21 == 1 & x22 == 0] * null
-tau_true[x21 == 1 & x22 == 1] <- w1[x21 == 1 & x22 == 1] * -effect_size
+tau_true[x3 == 0 & x4 == 0] <- w1[x3 == 0 & x4 == 0] * effect_size
+tau_true[x3 == 0 & x4 == 1] <- w1[x3 == 0 & x4 == 1] * null
+tau_true[x3 == 1 & x4 == 0] <- w1[x3 == 1 & x4 == 0] * null
+tau_true[x3 == 1 & x4 == 1] <- w1[x3 == 1 & x4 == 1] * -effect_size
 
 
 # num of obs. with effect 2: 
-length(y1[x21 == 0 & x22 == 0])
+length(y1[x3 == 0 & x4 == 0])
 
 # num of obs. with effect 0: 
-length(y1[x21 == 0 & x22 == 1]) + length(y1[x21 == 1 & x22 == 0])
+length(y1[x3 == 0 & x4 == 1]) + length(y1[x3 == 1 & x4 == 0])
 
 # num of obs. with effect -2: 
-length(y1[x21 == 1 & x22 == 1])
+length(y1[x3 == 1 & x4 == 1])
 
 
 # Unit level observed exposure and observed response
@@ -227,7 +230,7 @@ rpart.plot(bcf_fit.tree)
 
 # posterior splitting probabilities
 post_split_probs <- colMeans(s_bcf_itt.tree$varprb_tau)
-# check if x21 and x22 get recognized 
+# check if x3 and x4 get recognized 
 plot(post_split_probs)
 # transfer probabilities into variable-specific costs
 # These costs are scalings to be applied when considering splits,
@@ -253,7 +256,7 @@ rpart.plot(s_bcf_fit.tree)
 
 ##### Discovery with randomForest as in CRE #### 
 
-max_depth = 3
+max_depth = 2
 max_rules = 50
 forest <- randomForest::randomForest(y=s_bcf_tauhat,
                            x=s_bcf_exp[,-1],
@@ -494,6 +497,209 @@ select_rules <- function(rules_matrix, rules, ite,
       rule_LASSO <- rule_LASSO[order(-rule_LASSO[, 2]), ]
       rule_LASSO <- rule_LASSO[!is.na(rule_LASSO$rules), ]
       rules <- rule_LASSO$rules
+    
+      }
+    
+   else if (stability_selection=="softbart") {
+    # SoftBart
+    sbart_model <- SoftBart::softbart(X=rules_matrix,
+                                      X_test=rules_matrix,
+                                      Y=ite - mean(ite))
+    p_probs <- SoftBart::posterior_probs(sbart_model)
+    median_prob_model <- p_probs$median_probability_model
+    
+    ordered_rules <- data.frame(rules=rules, post_probs=p_probs$post_probs)
+    rules <- list(selected_rules=rules[median_prob_model], 
+                  post_probs_all=ordered_rules[order(ordered_rules$post_probs, decreasing = T),])
+   }
+  } else {
+    rules <- NULL
+  }
+  
+  #logger::log_debug("Done with selecting rules.")
+  return(rules)
+}
+
+
+rules <- select_rules(rules_matrix, rules, s_bcf_tauhat,
+                      stability_selection="softbart", cutoff, pfer, B)
+
+
+
+
+##### Discovery with randomForest as in CRE and SoftBart but without filter-functions#### 
+
+max_depth = 2
+max_rules = 50
+forest <- randomForest::randomForest(y=s_bcf_tauhat,
+                                     x=s_bcf_exp[,-1],
+                                     sampsize=0.5 * dim(s_bcf_exp[,-1])[1], 
+                                     ntree = 10,
+                                     maxnodes=2^max_depth,
+                                     nodesize=20,
+                                     mtry=ncol(s_bcf_exp[,-1])*2/3)
+
+
+treelist <- inTrees::RF2List(forest)
+
+#### initial set of decision rules ####
+
+extract_rules <- function(treelist, X, max_depth, digits = 2) {
+  
+  if (is.numeric(digits)) digits <- as.integer(abs(digits))
+  levelX <- list()
+  for (iX in 1:ncol(X)) levelX <- c(levelX, list(levels(X[, iX])))
+  ntree <- min(treelist$ntree)
+  allRulesList <- list()
+  for (iTree in 1:ntree) {
+    rule <- list()
+    count <- 0
+    rowIx <- 1
+    tree <- treelist$list[[iTree]]
+    if (nrow(tree) <= 1) next # skip if there is no split
+    ruleSet <- vector("list", length(which(tree[, "status"] == -1)))
+    for (max_length in 1:max_depth) {
+      res <- inTrees::treeVisit(tree,
+                                rowIx = rowIx,
+                                count,
+                                ruleSet,
+                                rule,
+                                levelX,
+                                length = 0,
+                                max_length = max_length,
+                                digits = digits)
+      allRulesList <- c(allRulesList, res$ruleSet)
+    }
+  }
+  
+  allRulesList <- allRulesList[!unlist(lapply(allRulesList, is.null))]
+  rules <- inTrees::ruleList2Exec(X, allRulesList)
+  return(rules)
+}
+rules <- extract_rules(treelist, s_bcf_exp[,-1], max_depth)
+rule_counts <- table(unlist(rules))
+M <- min(max_rules, length(rule_counts))
+rules <- names(sort(rule_counts, decreasing = TRUE)[1:M])
+M_initial <- length(rules)
+
+generate_rules_matrix <- function(X, rules_list) {
+  
+  # Generate and Rules Matrix
+  samplesize <- dim(X)[1]
+  nrules <- length(rules_list)
+  rules_matrix <- matrix(0, nrow = samplesize, ncol = nrules)
+  for (i in 1:nrules){
+    rules_matrix[eval(parse(text = rules_list[i]), list(X = X)), i] <- 1
+  }
+  return(rules_matrix)
+}
+standardize_rules_matrix <- function(rules_matrix) {
+  
+  samplesize <- dim(rules_matrix)[1]
+  nrules <- dim(rules_matrix)[2]
+  mu_rules_matrix <- apply(rules_matrix, 2, mean)
+  sd_rules_matrix <- apply(rules_matrix, 2, stats::sd)
+  rules_matrix_std <- matrix(0, samplesize, nrules)
+  for (l in 1:ncol(rules_matrix_std)) {
+    rules_matrix_std[, l] <- ((rules_matrix[, l] - mu_rules_matrix[l]) /
+                                sd_rules_matrix[l])
+  }
+  
+  return(rules_matrix_std)
+}
+rules_matrix <- generate_rules_matrix(s_bcf_exp[,-1], rules)
+
+
+#### select relevant rules via post lasso ####
+
+#stability_selection = "vanilla"
+cutoff = 0.6
+pfer = 1
+B = 20
+select_rules <- function(rules_matrix, rules, ite,
+                         stability_selection, cutoff, pfer, B) {
+  
+  #logger::log_debug("Selecting rules...")
+  
+  "%>%" <- magrittr::"%>%"
+  
+  rules_weight <- c()
+  for (rule in rules) {
+    rule_length <- lengths(regmatches(rule, gregexpr("&", rule))) + 1
+    rules_weight <- append(rules_weight, rule_length)
+  }
+  R <- t(t(rules_matrix) / rules_weight)
+  M <- ncol(R)
+  
+  if (length(rules) > 1) {
+    
+    if (stability_selection=="softbart_vanilla") {
+      # Softbart Vanilla Stability Selection
+      stability_scores <- rep(0, M)
+      ite_mean <- mean(ite)
+      for (i in 1:B) {
+        subsample <- 0.5
+        indices <- sample(1:nrow(R),
+                          size = round(nrow(R) * subsample),
+                          replace = FALSE)
+        sbart_model <- SoftBart::softbart(X=as.matrix(R[indices, ]),
+                           X_test=as.matrix(R[-indices, ]),
+                           Y=ite[indices] - mean(ite))
+        p_probs <- SoftBart::posterior_probs(sbart_model)
+        median_prob_model <- p_probs$median_probability_model
+        stability_scores[median_prob_model] <- stability_scores[median_prob_model] + 1
+      }
+      stability_scores <- stability_scores / B
+      rules <- rules[stability_scores >= cutoff]
+      
+    } else if (stability_selection=="error_control") {
+      # Stability Selection with Error Control
+      stab_mod <- tryCatch(
+        {
+          stabs::stabsel(x = as.data.frame(R),
+                         y = ite - mean(ite),
+                         intercept = FALSE,
+                         fitfun = "glmnet.lasso",
+                         cutoff = cutoff,
+                         PFER = pfer)
+        },
+        error = function(e) {
+          stop(paste(
+            "Combination of `cutoff` and `pfer` not allowed. ",
+            "Try to decrease the `cutoff` or increase the `pfer`. ",
+            "See Stability Selection documentation for further details.",
+            "\n\nOriginal Error message:", e))
+        }
+      )
+      rules <- rules[stab_mod$selected]
+      
+    } else if (stability_selection=="no") {
+      # LASSO
+      cv_lasso <- glmnet::cv.glmnet(x = rules_matrix,
+                                    y = ite - mean(ite),
+                                    alpha = 1,
+                                    intercept = FALSE)
+      aa <- stats::coef(cv_lasso, s = cv_lasso$lambda.1se)
+      index_aa <- which(aa[-1, 1] != 0)
+      rule_LASSO <- data.frame(rules = rules[index_aa],
+                               val = aa[index_aa + 1, 1])
+      rule_LASSO <- rule_LASSO[order(-rule_LASSO[, 2]), ]
+      rule_LASSO <- rule_LASSO[!is.na(rule_LASSO$rules), ]
+      rules <- rule_LASSO$rules
+      
+    }
+    
+    else if (stability_selection=="softbart") {
+      # SoftBart
+      sbart_model <- SoftBart::softbart(X=rules_matrix,
+                                        X_test=rules_matrix,
+                                        Y=ite - mean(ite))
+      p_probs <- SoftBart::posterior_probs(sbart_model)
+      median_prob_model <- p_probs$median_probability_model
+      
+      ordered_rules <- data.frame(rules=rules, post_probs=p_probs$post_probs)
+      rules <- list(selected_rules=rules[median_prob_model], 
+                    post_probs_all=ordered_rules[order(ordered_rules$post_probs, decreasing = T),])
     }
   } else {
     rules <- NULL
@@ -502,8 +708,10 @@ select_rules <- function(rules_matrix, rules, ite,
   #logger::log_debug("Done with selecting rules.")
   return(rules)
 }
+
+
 rules <- select_rules(rules_matrix, rules, s_bcf_tauhat,
-                      stability_selection="no", cutoff, pfer, B)
+                      stability_selection="softbart_vanilla", cutoff, pfer, B)
 
 
 
