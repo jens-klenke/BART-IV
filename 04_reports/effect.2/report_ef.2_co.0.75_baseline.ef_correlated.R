0bc19a1bb88aea6189fb8e058f6eeb0ebbc988e0
Klenke <57100601+jens-klenke@users.noreply.github.com>
2024-08-14 17:32:06 +0200

    new reports

---
+++ 04_reports/effect.2/report_ef.2_co.0.75_baseline.ef_correlated.R
@@ -0,0 +1,150 @@
+# load packages 
+############## Packages ################
+source(here::here('01_code/packages.R'))
+# some packages have to be implemented by hand (from github)
+
+# source all files in the functions folder
+invisible(sapply(list.files(here::here('01_code/functions'), full.names = TRUE), 
+                 source))
+
+# load data
+load(here::here('03_sim_results/effect.2/ef.2_co.0.75_baseline.ef_correlated.RData'))
+
+df <- sim_results %>% 
+  tidyr::unnest(results) %>%
+  # # name results 
+  dplyr::mutate(result_names = names(results)) %>%
+  # parse ID 
+  dplyr::mutate(ID = readr::parse_number(
+    str_extract(path_in, "_(?:.(?!_))+$"))) %>%
+  # delselect unimportant variables
+  dplyr::select(-c(row_num, path_in)) 
+
+## Theoretical results ----
+theoretical_df <- df %>%
+  dplyr::filter(stringi::stri_detect(result_names, regex = 'theoretical_results')) %>%
+  # unnest results to get the individual estimates 
+  tidyr::unnest(results) %>%
+  dplyr::mutate(CCACE = as.numeric(CCACE)) %>%
+  # group by method and dataset
+  dplyr::group_by(node, ncov)
+
+# Theoretical results (heterogenous groups hard coded not detected via binary tree)
+theoretical_df %>%
+  dplyr::summarise(mean = mean(CCACE), sd = sd(CCACE),
+                   median = median(CCACE), min = min(CCACE),
+                   max = max(CCACE))
+
+theoretical_df %>%
+  ggplot2::ggplot(aes(x = CCACE)) +
+  ggplot2::geom_density() +
+  ggplot2::facet_grid(ncov ~ node) +
+  ggplot2::theme_bw()
+
+
+## Frequentist IV results ----
+Freq_results <- df %>%
+  #  filter for main results
+  dplyr::filter(stringi::stri_detect(result_names, regex = 'bcf_results')) %>%
+  tidyr::unnest(results) %>%
+  # get model to distingush between bayes and frequentist 
+  dplyr::mutate(model = names(results)) %>%
+  # filter for freq results
+  dplyr::filter(model == 'bcfivResults') %>%
+  # force character datatype
+  dplyr::mutate(results = purrr::map(results, ~ dplyr::mutate(.x, node = as.character(node)))) %>%
+  dplyr::mutate(results = purrr::map(results, ~ dplyr::mutate(.x, CCACE = as.character(CCACE)))) %>%
+  dplyr::mutate(results = purrr::map(results, ~ dplyr::mutate(.x, pvalue = as.character(pvalue)))) %>%
+  dplyr::mutate(results = purrr::map(results, ~ dplyr::mutate(.x, Weak_IV_test = as.character(Weak_IV_test)))) %>%
+  dplyr::mutate(results = purrr::map(results, ~ dplyr::mutate(.x, Pi_obs = as.character(Pi_obs)))) %>%
+  dplyr::mutate(results = purrr::map(results, ~ dplyr::mutate(.x, ITT = as.character(ITT)))) %>%
+  dplyr::mutate(results = purrr::map(results, ~ dplyr::mutate(.x, Pi_compliers = as.character(Pi_compliers)))) %>%
+  # unnest bcf and bcfs results
+  tidyr::unnest(results) %>%
+  dplyr::arrange(ncov, ID) %>%
+  dplyr::mutate(subgroup = dplyr::case_when(
+    node == 'x1>=0.5 & x2>=0.5' | node == 'x2>=0.5 & x1>=0.5' |
+      node == 'x2> 0.5 & x1> 0.5' | node == 'x1> 0.5 & x2> 0.5' ~ 'negative effect',
+    node == 'x2< 0.5 & x1< 0.5' | node == 'x1< 0.5 & x2< 0.5' |
+      node == 'x2<=0.5 & x1<=0.5' | node == 'x1<=0.5 & x2<=0.5' ~ 'positive effect',
+    .default = NA)
+  )
+
+Freq_results %<>%
+  dplyr::filter(subgroup %in% c('negative effect', 'positive effect')) %>%
+  # force double datatype
+  dplyr::mutate(CCACE = as.double(CCACE),
+                pvalue = as.double(pvalue),
+                Weak_IV_test = as.double(Weak_IV_test),
+                Pi_obs = as.double(Pi_obs),
+                ITT = as.double(ITT),
+                Pi_compliers = as.double(Pi_compliers)
+  )
+
+Freq_results %>%
+  ggplot2::ggplot(aes(x = CCACE)) +
+  ggplot2::geom_density() +
+  ggplot2::facet_grid(ncov ~ subgroup + result_names) +
+  ggplot2::theme_bw() +
+  # Add number of observations as text
+  geom_text(data = get_counts(Freq_results), aes(x = Inf, y = Inf, label = paste("n =", n)),
+            hjust = 1.1, vjust = 1.1, size = 3, color = '#004c93') +
+  geom_vline(data = get_vline(Freq_results), aes(xintercept = x_line), color = '#004c93', linewidth = 0.75, linetype = 'longdash')
+
+# BCF poorly! SBCF better and almost unaffected by high number of covariates!
+Freq_results %>%
+  dplyr::filter(subgroup %in% c('negative effect', 'positive effect')) %>%
+  dplyr::group_by(subgroup, ncov, result_names) %>%
+  dplyr::summarise(n = dplyr::n(), share = dplyr::n()/100,
+                   median = median(CCACE), mean = mean(CCACE), sd = sd(CCACE)) %>% 
+  dplyr::arrange(result_names, subgroup, ncov)
+
+
+## Bayes IV results ----
+bayes_results <- df %>%
+  #  filter for main results
+  dplyr::filter(stringi::stri_detect(result_names, regex = 'bcf_results')) %>%
+  tidyr::unnest(results) %>%
+  # get model to distingush between bayes and frequentist 
+  dplyr::mutate(model = names(results)) %>%
+  # filter for freq results
+  dplyr::filter(model == 'bayes_ivResults') %>%
+  # unnest bcf and bcfs results
+  tidyr::unnest(results) %>%
+  dplyr::arrange(ncov, ID) %>%
+  dplyr::mutate(subgroup = dplyr::case_when(
+    node == 'x1>=0.5 & x2>=0.5' | node == 'x2>=0.5 & x1>=0.5' |
+      node == 'x2> 0.5 & x1> 0.5' | node == 'x1> 0.5 & x2> 0.5' ~ 'negative effect',
+    node == 'x2< 0.5 & x1< 0.5' | node == 'x1< 0.5 & x2< 0.5' |
+      node == 'x2<=0.5 & x1<=0.5' | node == 'x1<=0.5 & x2<=0.5' ~ 'positive effect',
+    .default = NA)
+  )
+
+bayes_results %<>%
+  dplyr::filter(subgroup %in% c('negative effect', 'positive effect')) %>%
+  # force double datatype
+  dplyr::mutate(CCACE = as.double(CCACE),
+                `CCACE l-95% CI` = as.double(`CCACE l-95% CI`),
+                `CCACE u-95% CI` = as.double(`CCACE u-95% CI`),
+                Pi_obs = as.double(Pi_obs),
+                ITT = as.double(ITT),
+                Pi_compliers = as.double(Pi_compliers)
+  )
+
+bayes_results %>%
+  ggplot2::ggplot(aes(x = CCACE)) +
+  ggplot2::geom_density() +
+  ggplot2::facet_grid(ncov ~ subgroup + result_names) +
+  ggplot2::theme_bw() +
+  # Add number of observations as text
+  geom_text(data = get_counts(bayes_results), aes(x = Inf, y = Inf, label = paste("n =", n)),
+            hjust = 1.1, vjust = 1.1, size = 3, color = '#004c93') +
+  geom_vline(data = get_vline(bayes_results), aes(xintercept = x_line), color = '#004c93', linewidth = 0.75, linetype = 'longdash')
+
+# BCF poorly! SBCF better and almost unaffected by high number of covariates!
+bayes_results %>%
+  dplyr::filter(subgroup %in% c('negative effect', 'positive effect')) %>%
+  dplyr::group_by(subgroup, ncov, result_names) %>%
+  dplyr::summarise(n = dplyr::n(), share = dplyr::n()/100,
+                   median = median(CCACE), mean = mean(CCACE), sd = sd(CCACE)) %>% 
+  dplyr::arrange(result_names, subgroup, ncov)
