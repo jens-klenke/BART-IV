e73355b6c62b43df39928ff67a3210f6cbc6b1e6
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2024-08-23 15:12:45 +0200

    . source

--- 01_code/functions/metric_functions.R
+++ 01_code/functions/metric_functions.R
@@ -3,3 +3,4 @@ bias_fun <- function(y_hat, y, ...) mean(y_hat -y)
 PEHE_fun <- function(y_hat, y, ...) mean( (y_hat - y)^2)
 coverage_fun <- function(...) 'Coveage needs to be implementet'
 
+## https://github.com/albicaron/SparseBCF/blob/main/Simulated%20examples/Section%205.1/AllR_Models_P25.R
