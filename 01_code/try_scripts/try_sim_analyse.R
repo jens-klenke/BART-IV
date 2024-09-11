a817563b8edb8ffa45f2f9cba0c12f11c358bf89
Klenke <57100601+jens-klenke@users.noreply.github.com>
2024-09-11 16:47:48 +0200

    Performance functions

---
+++ 01_code/try_scripts/try_sim_analyse.R
@@ -0,0 +1,69 @@
+# load packages 
+############## Packages ################
+source(here::here('01_code/packages.R'))
+# some packages have to be implemented by hand (from github)
+
+# source all files in the functions folder
+invisible(sapply(list.files(here::here('01_code/functions'), full.names = TRUE), 
+                 source))
+
+# load example data 
+load(here::here('est_problems.RData'))
+
+# 
+sim_results <- try_sim_results
+##
+# per row (with map)
+results_per_n <- sim_results$results[[1]]
+
+# bcf vs sbcf
+bcf_leave_results <- results_per_n$bcf_results
+s_bcf_leave_results <- results_per_n$s_bcf_results
+
+inference_results <- bcf_leave_results$bcfivResults
+bayes_results <- bcf_leave_results$bayes_ivResults
+
+# check if we have NO! estimation problems!
+check_inference <- ifelse(sum(inference_results$est_problems == 'yes') > 0, 
+                          FALSE, TRUE)
+check_bayes <- ifelse(sum(bayes_results$est_problems == 'yes') > 0, 
+                          FALSE, TRUE)
+
+# inference and bayes
+leaves_bcfiv_pred <- bcf_leave_results$bcfivResults %>%
+  dplyr::filter(leaves == 1) %>%
+  dplyr::select(pred) %>%
+  tidyr::unnest(pred)
+
+leave_result <- tibble::tibble(
+  # Model
+  model = 'bcf_inference', 
+  # Pehe leaves
+  pehe_leaves = PEHE_fun(leaves_bcfiv_pred$tau_true, leaves_bcfiv_pred$tau_pred),
+  # bias leaves
+  bias_leaves = bias_fun(leaves_bcfiv_pred$tau_true, leaves_bcfiv_pred$tau_pred),
+  # abs bias leaves
+  abs_bias_leaves = abs_bias_fun(leaves_bcfiv_pred$tau_true, leaves_bcfiv_pred$tau_pred),
+  # coverage leaves
+  coverage_leaves = mean(leaves_bcfiv_pred$coverage)
+)
+
+bcf_leave_results.bayes <- bcf_leave_results$bayes_ivResults
+
+
+# infernece
+leaves_bcfiv_pred <- s_leave_results$bcfivResults %>%
+  dplyr::filter(leaves == 1) %>%
+  dplyr::select(pred) %>%
+  tidyr::unnest(pred)
+
+levae_result <- tibble::tibble(
+  # Pehe leaves
+  pehe_leaves = PEHE_fun(leaves_bcfiv_pred$tau_true, leaves_bcfiv_pred$tau_pred),
+  # bias leaves
+  bias_leaves = bias_fun(leaves_bcfiv_pred$tau_true, leaves_bcfiv_pred$tau_pred),
+  # abs bias leaves
+  abs_bias_leaves = abs_bias_fun(leaves_bcfiv_pred$tau_true, leaves_bcfiv_pred$tau_pred),
+  # coverage leaves
+  coverage_leaves = mean(leaves_bcfiv_pred$coverage)
+)
